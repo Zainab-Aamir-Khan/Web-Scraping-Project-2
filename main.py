a741a6904c32770204d2620e9c265d2b2469ffd1
Zainab Aamir <zaamir427@gmail.com>
2025-01-31 22:44:10 +0500

    practice

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ url = requests.get("https://news.ycombinator.com/news").text
 soup = BeautifulSoup(url, 'lxml')
 
 # print(requests.get("https://news.ycombinator.com/news").status_code)
-mainContent = soup.find('table').text
+mainContent = soup.find('table')
 # print(mainContent)
 
 for main in mainContent.find_all('td', class_ = 'title'):
