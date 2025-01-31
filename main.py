1a347d88dee14b5603c0be1f33b3c4e08683ddf2
Zainab Aamir <zaamir427@gmail.com>
2025-01-31 22:46:21 +0500

    practice

--- main.py
+++ main.py
@@ -8,8 +8,10 @@ soup = BeautifulSoup(url, 'lxml')
 mainContent = soup.find('table')
 # print(mainContent)
 
-for main in mainContent.find_all('td', class_ = 'title'):
-    print(main.text)
+for news in mainContent.find_all('td', class_ = 'title'):
+    print(news.text)
+
+
 
     print(' ')
 
